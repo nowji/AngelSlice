d8bd35057d2ea0f3353d0391447671aed1e5688a
freakycows <nicofong99@gmail.com>
2025-10-21 19:29:25 -0400

    added weekly deals and fixed header

--- app/order/page.js
+++ app/order/page.js
@@ -51,10 +51,20 @@ export default function Order() {
       <div className="root font-germania min-h-screen flex flex-col">
           <header className="bg-[#671835] text-[#FFF7EB] px-6 py-3">
               <div className="flex justify-between items-center">
-                  <div>
-                      <h1 className="font-germania text-5xl md:text-5xl">Angel</h1>
-                      <h1 className="font-germania text-5xl md:text-5xl">Slice</h1>
-                  </div>
+                  <Link href="/" className="flex items-center gap-4 cursor-pointer hover:opacity-90 transition-opacity">
+                    <Image
+                        src="/logo.png"
+                        alt="Angel Slice logo"
+                        width={100}
+                        height={100}
+                        className="w-24 h-24"
+                    />
+                    <div>
+                        <h1 className="font-germania text-5xl md:text-5xl">Angel</h1>
+                        <h1 className="font-germania text-5xl md:text-5xl">Slice</h1>
+                    </div>
+                    </Link>
+                    
                   <Link href="/cart" className="cursor-pointer hover:opacity-80 transition-opacity mr-4 md:mr-8">
                       <Image
                           src="/checkout.png"
